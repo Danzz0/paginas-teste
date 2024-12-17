b5ebf7fc7149eddf5cc273f6293d4ff07dedfcf3
Danzz0 <danilo.limagomes10@gmail.com>
2024-12-17 11:00:53 -0300

    Criação da página 'Ciclo'

--- JS/index.js
+++ JS/index.js
@@ -1,1 +1,20 @@
-console.log("teste");
+document.addEventListener('DOMContentLoaded', function() {
+    const path = window.location.pathname;
+    console.log(path)
+  
+    // Verifica se a página é a desejada
+    if (path == '/HTML/ciclo.html') {
+      // Modifica o atributo de uma classe específica
+      const elements = document.getElementsByClassName("imgHome");
+      console.log(elements)
+
+      for(let i = 0; i < elements.length; i++){
+
+        elements[i].setAttribute('href', 'index.html');
+        elements[i].style.height = 'none';
+      } 
+    //   elements.forEach(element => {
+    //      // Substitua 'meuAtributo' e 'novoValor' pelos valores desejados
+    //   });
+    }
+  });
